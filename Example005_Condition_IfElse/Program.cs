d489ad461c679a864ee2acbddb41071345ea957f
SpotRus <spotik56rus@gmail.com>
2023-09-19 22:43:08 +0300

    Example 005 Condition If-Else

---
+++ Example005_Condition_IfElse/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите имя пользовотеля: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "маша")
+{
+    Console.WriteLine("УРА это же МАША!");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
