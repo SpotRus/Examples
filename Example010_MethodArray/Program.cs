714ca81efa56ff0fcff87a1ffbfcc7eb02fb0e38
SpotRus <spotik56rus@gmail.com>
2023-09-27 17:16:34 +0300

    Example 010 Method Array

---
+++ Example010_MethodArray/Program.cs
@@ -0,0 +1,16 @@
+﻿int[] array = { 21, 2, 6, 778, 88, 2, 34, 187 };
+
+int n = array.Length;
+int find = 2;
+
+int index = 0;
+
+while (index < n)
+{
+    if (array[index] == find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+    index++;
+}
